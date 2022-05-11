d02952bb1e9255fb48a857a8d42a08c8bafef978
Juan Pablo <Juan Pablo@LAPTOP-SOCJD2T1>
2022-05-11 15:44:03 -0500

    Una

---
+++ src/main/java/com/juanpa/PrimerMVC/Controllers/Demo.java
@@ -0,0 +1,13 @@
+package com.juanpa.PrimerMVC.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Demo {
+	@GetMapping("/demo/")
+	public String Index()
+	{
+		return "demo/index";
+	}
+}
---
+++ src/main/java/com/juanpa/PrimerMVC/Controllers/HomeController.java
@@ -0,0 +1,17 @@
+package com.juanpa.PrimerMVC.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	
+	@GetMapping("/")
+	public String Saludo() {
+		return "ejemplo";
+	}
+	@GetMapping("/Ejemplo1")
+	public String Ejemplo1() {
+		return "ejemplo1";
+	}
+}
